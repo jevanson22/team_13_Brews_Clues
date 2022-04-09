6aaeb22c1c2f10a41f62ff4d96aa7bd6debe5645
jevanson22 <101901022+jevanson22@users.noreply.github.com>
2022-04-09 15:53:29 -0700

    hope this works

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+google.charts.load('current', {
+    'packages': ['corechart']
+  });
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Beer', 8],
+      ['Whiskey', 1],
+      ['Wine', 11],
+      ['Zima-we know you exist, we still see it in gas station refrigerators by the pickled eggs', 1],
+      ['To young to drink/whatever was on sale', 4]
+    ]);
+
+    var options = {
+      title: 'Most Common booze bootcamp students keep around'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+  }
